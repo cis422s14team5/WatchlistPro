b420dac2317e80c4433a9ff08f30dd590287d959
Keith Hamm <hkhamm@gmail.com>
2014-05-22 09:31:39 -0700

    updated client with tmdb

--- src/client/TopicHandler.java
+++ src/client/TopicHandler.java
@@ -156,91 +156,9 @@ public class TopicHandler {
         return output;
     }
 
-    public List<String> tvOutput(JSONObject topic) {
+    public List<String> tvOutput(JSONObject jsonObject) {
         List<String> output = new ArrayList<>();
 
-        String title;
-        try {
-            title = JsonPath.read(topic, "$.property['/type/object/name'].values[0].value").toString();
-        } catch (Exception e) {
-            title = "";
-        }
-
-        JSONArray genres;
-        try {
-            genres = JsonPath.read(topic, "$.property['/tv/tv_program/genre'].values");
-        } catch (Exception e) {
-            genres = new JSONArray();
-        }
-        ArrayList<String> genreList = new ArrayList<>();
-        if (genres.size() > 0) {
-            for (Object obj : genres) {
-                JSONObject genre = (JSONObject) obj;
-                genreList.add(genre.get("text").toString());
-            }
-        } else {
-            genreList.add("");
-        }
-
-        JSONArray creators;
-        try {
-            creators = JsonPath.read(topic, "$.property['/tv/tv_program/program_creator'].values");
-        } catch (Exception e) {
-            creators = new JSONArray();
-        }
-        ArrayList<String> creatorList = new ArrayList<>();
-        if (creators.size() > 0) {
-            for (Object obj : creators) {
-                JSONObject creator = (JSONObject) obj;
-                creatorList.add(creator.get("text").toString());
-            }
-        } else {
-            creatorList.add("");
-        }
-
-        String network;
-        try {
-            JSONObject networkTemp = JsonPath.read(topic,
-                    "$.property['/tv/tv_program/original_network'].values[0]");
-            network = JsonPath.read(networkTemp,
-                    "$.property['/tv/tv_network_duration/network'].values[0].text");
-        } catch (InvalidPathException e) {
-            network = "";
-        }
-
-        //String runtime = JsonPath.read(topic, "$.property['/tv/tv_program/runtime'].values[0].text").toString();
-
-        JSONArray descriptions;
-        try {
-            descriptions = JsonPath.read(topic, "$.property['/common/topic/description'].values");
-        } catch (Exception e) {
-            descriptions = new JSONArray();
-        }
-        ArrayList<String> descriptionList = new ArrayList<>();
-        if (descriptions.size() > 0) {
-            for (Object obj : descriptions) {
-                JSONObject description = (JSONObject) obj;
-                descriptionList.add(description.get("value").toString());
-            }
-        } else {
-            descriptionList.add("");
-        }
-
-        String numSeasons;
-        try {
-            numSeasons = String.format("%s", (int) Float.parseFloat(JsonPath.read(topic,
-                    "$.property['/tv/tv_program/number_of_seasons'].values[0].value").toString()));
-        } catch (NumberFormatException e) {
-            numSeasons = "";
-        }
-
-        String numEpisodes;
-        try {
-            numEpisodes = JsonPath.read(topic,
-                    "$.property['/tv/tv_program/number_of_episodes'].values[0].value").toString();
-        } catch (Exception e) {
-            numEpisodes = "";
-        }
 
 //        JSONArray episodes =
 //                JsonPath.read(topic,"$.property['/tv/tv_program/episodes'].values");
@@ -260,48 +178,173 @@ public class TopicHandler {
 //        }
 
         output.clear();
-        output.add(title);
+        output.add(jsonObject.get("title").toString());
         output.add("No"); // Watched.
+        output.add(jsonObject.get("genre").toString());
+        output.add(jsonObject.get("creator").toString());
 
-        String genreTemp = "";
-        for (int i = 0; i < genreList.size(); i++) {
-            genreTemp += genreList.get(i);
-            if (i != genreList.size() - 1) {
-                genreTemp += ", ";
-            }
+        output.add(jsonObject.get("network").toString());
+
+        output.add(jsonObject.get("runtime").toString()); // runtime
+
+        output.add(String.format(jsonObject.get("seasons").toString()));
+        output.add(jsonObject.get("seasons").toString());
+        output.add(jsonObject.get("description").toString());
+
+        int num = (int) jsonObject.get("seasons");
+        for (int i = 0; i < num; i++) {
+            output.add(jsonObject.get(i).toString());
         }
-        output.add(genreTemp);
-
-        String creatorsTemp = "";
-        for (int i = 0; i < creatorList.size(); i++) {
-            creatorsTemp += creatorList.get(i);
-            if (i != creatorList.size() - 1) {
-                creatorsTemp += ", ";
-            }
-        }
-        output.add(creatorsTemp);
-
-        output.add(String.format(network));
-
-        output.add(""); // runtime
-
-        output.add(String.format(numSeasons));
-        if (!numEpisodes.equals("[]")) {
-            int num = (int) Float.parseFloat(numEpisodes);
-            output.add(Integer.toString(num));
-        } else {
-            output.add("");
-        }
-
-        String descriptionTemp = "";
-        for (int i = 0; i < descriptionList.size(); i++) {
-            descriptionTemp += descriptionList.get(i);
-            if (i != descriptionList.size() - 1) {
-                descriptionTemp += ", ";
-            }
-        }
-        output.add(descriptionTemp);
 
         return output;
     }
+
+//    public List<String> tvOutput(JSONObject topic) {
+//        List<String> output = new ArrayList<>();
+//
+//        String title;
+//        try {
+//            title = JsonPath.read(topic, "$.property['/type/object/name'].values[0].value").toString();
+//        } catch (Exception e) {
+//            title = "";
+//        }
+//
+//        JSONArray genres;
+//        try {
+//            genres = JsonPath.read(topic, "$.property['/tv/tv_program/genre'].values");
+//        } catch (Exception e) {
+//            genres = new JSONArray();
+//        }
+//        ArrayList<String> genreList = new ArrayList<>();
+//        if (genres.size() > 0) {
+//            for (Object obj : genres) {
+//                JSONObject genre = (JSONObject) obj;
+//                genreList.add(genre.get("text").toString());
+//            }
+//        } else {
+//            genreList.add("");
+//        }
+//
+//        JSONArray creators;
+//        try {
+//            creators = JsonPath.read(topic, "$.property['/tv/tv_program/program_creator'].values");
+//        } catch (Exception e) {
+//            creators = new JSONArray();
+//        }
+//        ArrayList<String> creatorList = new ArrayList<>();
+//        if (creators.size() > 0) {
+//            for (Object obj : creators) {
+//                JSONObject creator = (JSONObject) obj;
+//                creatorList.add(creator.get("text").toString());
+//            }
+//        } else {
+//            creatorList.add("");
+//        }
+//
+//        String network;
+//        try {
+//            JSONObject networkTemp = JsonPath.read(topic,
+//                    "$.property['/tv/tv_program/original_network'].values[0]");
+//            network = JsonPath.read(networkTemp,
+//                    "$.property['/tv/tv_network_duration/network'].values[0].text");
+//        } catch (InvalidPathException e) {
+//            network = "";
+//        }
+//
+//        //String runtime = JsonPath.read(topic, "$.property['/tv/tv_program/runtime'].values[0].text").toString();
+//
+//        JSONArray descriptions;
+//        try {
+//            descriptions = JsonPath.read(topic, "$.property['/common/topic/description'].values");
+//        } catch (Exception e) {
+//            descriptions = new JSONArray();
+//        }
+//        ArrayList<String> descriptionList = new ArrayList<>();
+//        if (descriptions.size() > 0) {
+//            for (Object obj : descriptions) {
+//                JSONObject description = (JSONObject) obj;
+//                descriptionList.add(description.get("value").toString());
+//            }
+//        } else {
+//            descriptionList.add("");
+//        }
+//
+//        String numSeasons;
+//        try {
+//            numSeasons = String.format("%s", (int) Float.parseFloat(JsonPath.read(topic,
+//                    "$.property['/tv/tv_program/number_of_seasons'].values[0].value").toString()));
+//        } catch (NumberFormatException e) {
+//            numSeasons = "";
+//        }
+//
+//        String numEpisodes;
+//        try {
+//            numEpisodes = JsonPath.read(topic,
+//                    "$.property['/tv/tv_program/number_of_episodes'].values[0].value").toString();
+//        } catch (Exception e) {
+//            numEpisodes = "";
+//        }
+//
+////        JSONArray episodes =
+////                JsonPath.read(topic,"$.property['/tv/tv_program/episodes'].values");
+////        ArrayList<String> episodeList = new ArrayList<>();
+////        for (Object obj : episodes) {
+////            JSONObject episode = (JSONObject) obj;
+////            episodeList.add(episode.get("text").toString());
+////        }
+//
+////        String website;
+////        try {
+////            website =
+////                    JsonPath.read(topic,
+////                            "$.property['/common/topic/official_website'].values[0].value").toString();
+////        } catch (com.jayway.jsonpath.InvalidPathException e) {
+////            website = "No website listed.";
+////        }
+//
+//        output.clear();
+//        output.add(title);
+//        output.add("No"); // Watched.
+//
+//        String genreTemp = "";
+//        for (int i = 0; i < genreList.size(); i++) {
+//            genreTemp += genreList.get(i);
+//            if (i != genreList.size() - 1) {
+//                genreTemp += ", ";
+//            }
+//        }
+//        output.add(genreTemp);
+//
+//        String creatorsTemp = "";
+//        for (int i = 0; i < creatorList.size(); i++) {
+//            creatorsTemp += creatorList.get(i);
+//            if (i != creatorList.size() - 1) {
+//                creatorsTemp += ", ";
+//            }
+//        }
+//        output.add(creatorsTemp);
+//
+//        output.add(String.format(network));
+//
+//        output.add(""); // runtime
+//
+//        output.add(String.format(numSeasons));
+//        if (!numEpisodes.equals("[]")) {
+//            int num = (int) Float.parseFloat(numEpisodes);
+//            output.add(Integer.toString(num));
+//        } else {
+//            output.add("");
+//        }
+//
+//        String descriptionTemp = "";
+//        for (int i = 0; i < descriptionList.size(); i++) {
+//            descriptionTemp += descriptionList.get(i);
+//            if (i != descriptionList.size() - 1) {
+//                descriptionTemp += ", ";
+//            }
+//        }
+//        output.add(descriptionTemp);
+//
+//        return output;
+//    }
 }
